d2cd13ec2d34d329d1af8ceb6fb43cf1697f84c6
ReneeDanielle96 <119815093+ReneeDanielle96@users.noreply.github.com>
2023-03-30 00:16:50 +0100

    made code structure changes

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,8 +1,7 @@
 //Retrieve highscores from local storage
 function displayHighScores () {
-    var scores = JSON.parse(localStorage.getItem("highscores")) || [];
+    var scores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 }
-
 
 //Add highscores history with recent scores first
 highcores.sort(function (a, b) {
@@ -13,17 +12,16 @@ highcores.sort(function (a, b) {
 highcores.forEach(function (score) {
     var li = document.createElement("li");
     li.textContent = score.initials + " ~ " + score.score;
+
+    //Display highscore on page
+    var ol = document.getElementById("highscores");
+    ol.appendChild(li);
 });
-
-//Apend highscore on page
-var ol = document.getElementById("highscores");
-ol.appendChild(li);
-
 
 //Clear highscores
 function deleteHighScores () {
     window.localStorage.removeItem("highscores");
-    location.reload();
+    window.location.reload();
 }
 document.getElementById("clear").onclick = deleteHighScores;
 
